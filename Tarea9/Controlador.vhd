7d1ca136c33d49c62a91afcd07e3d95bdacaa21c
Marc Clasca <marc.clasca@bsc.es>
2021-02-28 22:05:33 +0100

    Errors de sintaxis arreglats

--- Tarea9/Controlador.vhd
+++ Tarea9/Controlador.vhd
@@ -1,10 +1,11 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
+
 ENTITY Controlador IS
 	PORT (
 		Clk  : IN std_logic;
-		KEY0 : IN std_logic_vector(0 DOWNTO 0);
-		KEY1 : IN std_logic_vector(0 DOWNTO 0);
+		KEY0 : IN std_logic;
+		KEY1 : IN std_logic;
 		acabat: IN std_logic;
 		LEDG : OUT std_logic;
 		run  : OUT std_logic;
@@ -17,8 +18,8 @@ signal estat, prxestat : tipoestat := REPOS;
 BEGIN
 	
 -- Logica de proxim estat
-	prxestat <= IMPRIMINT when estat = REPOS and rising_edge(KEY1(0))
-		else REPOS when estat = IMPRIMINT and (rising_edge KEY0(0) or rising_edge(acabat));
+	prxestat <= IMPRIMINT when estat = REPOS and rising_edge(KEY1) else 
+					REPOS when estat = IMPRIMINT and (rising_edge(KEY0) or rising_edge(acabat));
 
 -- Accions d'estat 
 	LEDG <= '1' when estat = REPOS else '0' when estat = IMPRIMINT;
@@ -26,5 +27,5 @@ BEGIN
 	parar <= '1' when estat = REPOS else '0' when estat = IMPRIMINT;
 	
 
-	estat <= proximestat when rising_edge(Clk);
+	estat <= prxestat when rising_edge(Clk);
 END Structure;
