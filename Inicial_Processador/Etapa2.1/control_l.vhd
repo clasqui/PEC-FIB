0f3cc63e89d1017729245516e915504086f2fbed
Quim Ferrer <quimfersa@hotmail.com>
2021-03-20 23:30:28 +0100

    Passem joc de proves 2.1. Cal proposar nou

--- Inicial_Processador/Etapa2.1/control_l.vhd
+++ Inicial_Processador/Etapa2.1/control_l.vhd
@@ -29,6 +29,12 @@ BEGIN
 	 --op <= "01" when ir(15 downto 12) = "0011" or ir(15 downto 12) = "0100" or ir(15 downto 12) = "1101" or ir(15 downto 12) = "1110"
 				--else "0"&ir(8);
 	
+	with ir(15 downto 12) select wrd <=
+		'1' when "0101", --	 MOVHI/MOVI
+		'1' when "0011", -- LD
+		'1' when "1101", -- LDB
+		'0'when others; -- CASOS ST LD
+	
 	-- TODO: fer signal 
 	 with ir(15 downto 12) select op <=
 		'0'&ir(8) when "0101", --	 ARREGLAR AIXO EN UN FUTUR (CAS MOVI / MOVHI)
@@ -36,12 +42,12 @@ BEGIN
 	 
 	 
 	 addr_d <= ir(11 downto 9);
-	 addr_a <= ir(11 downto 9) when ir(15 downto 12) = "0101" -- EN LES dimmediat, la a esta aqui
+	 addr_a <= ir(11 downto 9) when ir(15 downto 12) = "0101"
 					else ir(8 downto 6);
 	 addr_b <= ir(11 downto 9);
 	 
-	 wrd <= ir(12);
-	 wr_m <= '1' when ir(12) = '0';
+	 -- wrd <= ir(12);
+	 wr_m <= '1' when ir(12) = '0' else '0';
 	 
 	 in_d <= '1' when ir(15 downto 12) = "0011" or ir(15 downto 12) = "1101" else '0';
 	 
