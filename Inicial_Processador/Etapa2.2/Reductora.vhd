f8a1e71598cf3e7ef42e3fd22a2eefdac270f1fe
Quim Ferrer <quimfersa@hotmail.com>
2021-04-07 19:05:13 +0200

    Reductora amb nomes un process

--- Inicial_Processador/Etapa2.2/Reductora.vhd
+++ Inicial_Processador/Etapa2.2/Reductora.vhd
@@ -1,6 +1,6 @@
--- És el rellotge de la tarea7 convertit en una reductora.
+-- Ã‰s el rellotge de la tarea7 convertit en una reductora.
 
--- SERIA ESSENCIAL CANVIAR AIXÒ PER USAR EL PLL DE LA PLACA I NO UN CONTADOR PERO DE MOMENT TENIM AIXÒ.
+-- SERIA ESSENCIAL CANVIAR AIXÃ’ PER USAR EL PLL DE LA PLACA I NO UN CONTADOR PERO DE MOMENT TENIM AIXÃ’.
 
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
@@ -8,7 +8,7 @@ USE IEEE.std_logic_arith.ALL;
 USE ieee.std_logic_unsigned.ALL;
 
 ENTITY Reductora IS
-GENERIC (reductora : integer := 1);   -- Per defecte no hi ha reducció.
+GENERIC (reductora : integer := 1);   -- Per defecte no hi ha reducciÃ³.
 PORT (
 		CLOCK_50 : IN std_logic;
 		rellotge : OUT std_logic
@@ -20,17 +20,22 @@ ARCHITECTURE Structure OF Reductora IS
 
 	type estat_t is (BAIX, ALT);
 
-	signal contador : integer := reductora;
-	signal estat : estat_t := ALT;
+	signal contador : integer := reductora-1;
+	signal estat : estat_t := BAIX;
 	signal pols : std_logic;
 BEGIN
-	pols <= '1' when contador = 0 else '0';
+	-- pols <= '1' when contador = 0 else '0';
 	
 	
 	process(CLOCK_50)
 	begin
 		if contador = 0 then
 			contador <= reductora-1;
+			if estat = BAIX then
+				estat <= ALT;
+			else
+				estat <= BAIX;
+			end if;
 		else 
 			contador <= contador - 1;
 		end if;
@@ -38,17 +43,17 @@ BEGIN
 	
 	rellotge <= '1' when estat = ALT else '0';
 	
-	-- Lògica d'estat
-	process (pols)
-	begin
-		if rising_edge(pols) then
-			if estat = BAIX then
-				estat <= ALT;
-			else
-				estat <= BAIX;
-			end if;			
-		end if;
-	end process;
+--	-- LÃ²gica d'estat
+--	process (pols)
+--	begin
+--		if rising_edge(pols) then
+--			if estat = BAIX then
+--				estat <= ALT;
+--			else
+--				estat <= BAIX;
+--			end if;			
+--		end if;
+--	end process;
 		
 	
 END Structure;
