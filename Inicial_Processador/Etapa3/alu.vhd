89c0f9e8a928e745ae9332be82a1442200a39a30
Marc Clasca <marc.clasca@bsc.es>
2021-04-08 13:22:39 +0200

    (casi) ops implementades a la alu

--- Inicial_Processador/Etapa3/alu.vhd
+++ Inicial_Processador/Etapa3/alu.vhd
@@ -41,6 +41,23 @@ BEGIN
 		
 		std_logic_vector(signed(y)+signed(x)) when "00110",	-- ADD/ADDI/LOAD/STORE
 		
+		y and x when "00010", -- AND
+		y or x when "00011",  -- OR
+		y xor x when "00100", -- XOR
+		not x when "00101",   -- NOT
+		std_logic_vector(signed(x) - signed(y)) when "00111", -- SUB
+		(shift_left(signed(x), unsigned(y(4 downto 0))) when y(4) = '0' else -- SHA (esquerra, positiu)
+		shift_right(signed(x), (unsigned(not y(4 downto 0)) + 1))) when "01000", -- SHA (dreta, negatiu)
+		(shift_left(unsigned(x), unsigned(y(4 downto 0))) when y(4) = '0' else -- SHL (esquerra, positiu)
+		shift_right(unsigned(x), (unsigned(not y(4 downto 0)) + 1))) when "01001", -- SHL (dreta, negatiu)
+		("0000000000000001" when signed(x) < signed (y) else "0000000000000000") when "01010", -- CMPLT
+		("0000000000000001" when signed(x) < signed (y) or signed(x) = signed (y) else "0000000000000000") when "01011", --CMPLE
+		("0000000000000001" when signed(x) = signed (y) else "0000000000000000") when "01100", --CMPEQ
+		("0000000000000001" when unsigned(x) < unsigned (y) else "0000000000000000") when "01101", -- CMPLTU
+		("0000000000000001" when unsigned(x) < unsigned (y) or unsigned(x) = unsigned (y) else "0000000000000000") when "01110", --CMPLEU
+		
+		
+		
 		(others=>'0') when others;	
 
 END Structure;
