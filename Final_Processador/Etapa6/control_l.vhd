913f9f7d65d5b94984f203d87d8c87bdb97a367c
Quim Ferrer <quimfersa@hotmail.com>
2021-04-18 17:00:18 +0200

    Controlador teclat fet i debugat

---
+++ Final_Processador/Etapa6/control_l.vhd
@@ -0,0 +1,164 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE ieee.numeric_std.all;
+
+ENTITY control_l IS
+    PORT (ir        : IN  STD_LOGIC_VECTOR(15 DOWNTO 0);
+          op        : OUT STD_LOGIC_VECTOR(4 DOWNTO 0);
+          ldpc      : OUT STD_LOGIC;
+          wrd       : OUT STD_LOGIC;
+          addr_a    : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
+          addr_b    : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
+          addr_d    : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
+          immed     : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+          wr_m      : OUT STD_LOGIC;
+          in_d      : OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
+          immed_x2  : OUT STD_LOGIC;
+          word_byte : OUT STD_LOGIC;
+			 Rb_N		  : OUT STD_LOGIC;
+			 addr_io	  : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+			 rd_in	  : OUT STD_LOGIC;
+			 wr_out    : OUT STD_LOGIC);
+END control_l;
+
+
+ARCHITECTURE Structure OF control_l IS
+
+signal PC	: std_LOGIC_VECTOR(15 downto 0);
+signal arit	: std_LOGIC_VECTOR(4 downto 0);
+signal cmp	: std_LOGIC_VECTOR(4 downto 0);
+signal mult	: std_LOGIC_VECTOR(4 downto 0);
+signal jmp	: std_logic;
+signal io	: std_logic;
+
+BEGIN
+
+    -- Aqui iria la generacion de las senales de control del datapath
+	 ldpc <= '0' when ir = "1111111111111111" else '1';
+	 --op <= "01" when ir(15 downto 12) = "0011" or ir(15 downto 12) = "0100" or ir(15 downto 12) = "1101" or ir(15 downto 12) = "1110"
+				--else "0"&ir(8);
+	
+	
+	-- Senyal intermitja pel wrd.
+	
+	jmp <= '1' when ir(2 downto 0) = "100" else '0';
+	io <= not ir(8);
+	
+	with ir(15 downto 12) select wrd <=      -- Marquem com a 1 els casos en els que s'esriu perque no hi hagi lios al implementar noves instructs
+		'1' when "0101", -- MOVHI/MOVI
+		'1' when "0011", -- LD
+		'1' when "1101", -- LDB
+		'1' when "0000", -- aritmeticologiques
+		'1' when "0001", -- comparacions
+		'1' when "0010", -- ADDI
+		'1' when "1000", -- muls i divs
+		jmp when "1010",   -- Pels jumps
+		io  when "0111",
+		'0' when others; -- CASOS ST LD
+	
+-- CODIS D'OPERACIÓ
+	--	00000 MOVI
+	--	00001	MOVHI
+	--	00010	AND
+	--	00011 OR
+	--	00100 XOR
+	--	00101 NOT
+	--	00110 ADD/ADDI/LOAD/STORE
+	--	00111 SUB
+	--	01000 SHA
+	--	01001 SHL
+	--	01010 CMPLT
+	--	01011 CMPLE
+	--	01100 CMPEQ
+	--	01101 CMPLTU
+	--	01110 CMPLEU
+	--	01111 MUL
+	--	10000 MULH
+	--	10001 MULHU
+	--	10010 DIV
+	-- 10011	DIVU
+	-- 10100 JMP --> fa passar el registre a la sortida.
+	
+-- Senyals funció
+	 with ir(5 downto 3) select arit <=
+		"00010" when "000", -- AND
+		"00011" when "001", -- OR
+		"00100" when "010", -- XOR
+		"00101" when "011", -- NOT
+		"00110" when "100", -- AND
+		"00111" when "101", -- SUB
+		"01000" when "110", -- SHA
+		"01001" when "111", -- SHL
+		"XXXXX" when others; 
+		
+	with ir(5 downto 3) select cmp <=
+		"01010" when "000", -- CMPLT
+		"01011" when "001", -- CMPLE
+		"XXXXX" when "010", -- -
+		"01100" when "011", -- CMPEQ
+		"01101" when "100", -- CMPLTU
+		"01110" when "101", -- CMPLEU
+		"XXXXX" when "110", -- -
+		"XXXXX" when "111", -- -
+		"XXXXX" when others;
+		
+	with ir(5 downto 3) select mult <=
+		"01111" when "000", -- MUL
+		"10000" when "001", -- MULH
+		"10001" when "010", -- MULHU
+		"XXXXX" when "011", -- -
+		"10010" when "100", -- DIV
+		"10011" when "101", -- DIVU
+		"XXXXX" when "110", -- -
+		"XXXXX" when "111", -- -
+		"XXXXX" when others;
+	 
+
+-- Senyal op
+	 with ir(15 downto 12) select op <=
+		"0000"&ir(8) 	when "0101", -- MOVHI i MOVI
+		arit 				when "0000", -- Aritmeticologiques.
+		cmp				when "0001", -- Comparacions.
+		mult				when "1000", -- Multiplicacions i divisions
+		"10100"			when "1010", -- JMP
+ 		"00110"			when others; -- ADDI, Loads, Stores.
+		
+	 -- ATENCIO QUE CAL LA SENYAL Rb_N !!!!
+	 Rb_N <= '0' when ir(15 downto 12) = "0010" -- MOVHI/MOVI
+							or ir(15 downto 12) = "0101" -- 
+							or ir(15 downto 12) = "0011" 
+							or ir(15 downto 12) = "0100" 
+							or ir(15 downto 12) = "1101" 
+							or ir(15 downto 12) = "1110" 
+						else '1'; 
+	 
+	 addr_d <= ir(11 downto 9);  -- Rd sempre està al mateix lloc
+	 addr_a <= ir(11 downto 9) when ir(15 downto 12) = "0101"
+					else ir(8 downto 6); 
+	 addr_b <= ir(2 downto 0) when ir(15 downto 12) = "0000" 
+											or ir(15 downto 12) = "0001" 
+											or ir(15 downto 12) = "1000" -- Esta al darrere en arit. cmp. i mul.
+					else ir(11 downto 9);  -- Rb canvia de lloc.
+	 
+	
+	 wr_m <= '1' when ir(15 downto 12) = "0100" or ir(15 downto 12) = "1110" else '0';  -- Nomes els store han d'escriure memoria
+	 
+	 in_d <= "01" when ir(15 downto 12) = "0011" or ir(15 downto 12) = "1101" 
+							else "10" when ir(15 downto 12) = "1010" and ir(2 downto 0) = "100" 
+							else "11" when ir(15 downto 12) = "0111" else "00";
+	
+	-- Senyals per a l'I/O
+	 addr_io <= ir(7 downto 0);
+	 rd_in <= '1' when ir(15 downto 12) = "0111" and ir(8) = '0' else '0';
+	 wr_out <= '1' when ir(15 downto 12) = "0111" and ir(8) = '1' else '0';
+	 
+	 -- Altres
+	 immed_x2 <= '1' when ir(15 downto 12) = "0011" or ir(15 downto 12) = "0100" else '0';
+	 
+	 word_byte <= ir(15);
+	 
+	 immed <= std_logic_vector(resize(signed(ir(7 downto 0)), 16)) when ir(15 downto 12) = "0101" -- en les dimmediat, son 8 bits
+					else std_logic_vector(resize(signed(ir(5 downto 0)), 16)); -- en les altres, son 6 bits
+					
+	
+END Structure;
---
+++ Final_Processador/Etapa6/unidad_control.vhd
@@ -0,0 +1,159 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE ieee.numeric_std.all;
+USE ieee.std_logic_unsigned.all;
+
+ENTITY unidad_control IS
+    PORT (boot      : IN  STD_LOGIC;
+          clk       : IN  STD_LOGIC;
+          datard_m  : IN  STD_LOGIC_VECTOR(15 DOWNTO 0);
+          op        : OUT STD_LOGIC_VECTOR(4 DOWNTO 0);
+          wrd       : OUT STD_LOGIC;
+          addr_a    : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
+          addr_b    : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
+          addr_d    : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
+          immed     : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+          pc        : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+          ins_dad   : OUT STD_LOGIC;
+          in_d      : OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
+          immed_x2  : OUT STD_LOGIC;
+          wr_m      : OUT STD_LOGIC;
+          word_byte : OUT STD_LOGIC;
+			 Rb_N		  : OUT STD_LOGIC;
+			 z 		  : IN  STD_LOGIC;
+			 aluout	  : IN  STD_LOGIC_VECTOR(15 DOWNTO 0);
+			 addr_io	  : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+			 rd_in	  : OUT STD_LOGIC;
+			 wr_out    : OUT STD_LOGIC);
+END unidad_control;
+
+ARCHITECTURE Structure OF unidad_control IS
+
+type tknbr_t is (INI,PC_ACT,PC2,ALU_OUT,BR);
+
+signal ldpc: std_logic;
+signal nou_pc: std_logic_vector(15 downto 0);
+signal pc_saltat: std_logic_vector(15 downto 0);
+SIGNAL ir_actual: std_logic_vector(15 downto 0);
+signal ldpc_l : std_logic;
+signal wrd_l : std_logic;
+signal w_b : std_logic;
+signal wr_m_l : std_logic;
+signal ldir : std_logic;
+signal tknbr: tknbr_t := INI;
+signal jmp: tknbr_t;
+	 
+COMPONENT control_l IS
+    PORT (ir   	  : IN  STD_LOGIC_VECTOR(15 DOWNTO 0);
+          op        : OUT STD_LOGIC_VECTOR(4 DOWNTO 0);
+          ldpc      : OUT STD_LOGIC;
+          wrd       : OUT STD_LOGIC;
+          addr_a    : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
+          addr_b    : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
+          addr_d    : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
+          immed     : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+          wr_m      : OUT STD_LOGIC;
+          in_d      : OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
+          immed_x2  : OUT STD_LOGIC;
+          word_byte : OUT STD_LOGIC;
+			 Rb_N		  : OUT STD_LOGIC;
+			 addr_io	  : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+			 rd_in	  : OUT STD_LOGIC;
+			 wr_out    : OUT STD_LOGIC);
+END COMPONENT;
+
+COMPONENT multi IS
+    port(clk       : IN  STD_LOGIC;
+         boot      : IN  STD_LOGIC;
+         ldpc_l    : IN  STD_LOGIC;
+         wrd_l     : IN  STD_LOGIC;
+         wr_m_l    : IN  STD_LOGIC;
+         w_b       : IN  STD_LOGIC;
+         ldpc      : OUT STD_LOGIC;
+         wrd       : OUT STD_LOGIC;
+         wr_m      : OUT STD_LOGIC;
+         ldir      : OUT STD_LOGIC;
+         ins_dad   : OUT STD_LOGIC;
+         word_byte : OUT STD_LOGIC);
+END COMPONENT;
+
+
+BEGIN
+
+    -- Aqui iria la declaracion del "mapeo" (PORT MAP) de los nombres de las entradas/salidas de los componentes
+    -- En los esquemas de la documentacion a la instancia de la logica de control le hemos llamado c0
+    -- Aqui iria la definicion del comportamiento de la unidad de control y la gestion del PC
+
+	 c0 : control_l PORT MAP (
+		ir => ir_actual,
+		op => op,
+		ldpc => ldpc_l,
+		wrd => wrd_l,
+		addr_a => addr_a,
+		addr_b => addr_b,
+		addr_d => addr_d,	
+		wr_m => wr_m_l,
+		immed => immed,
+		in_d => in_d,
+		immed_x2 => immed_x2,
+		word_byte => w_b,
+		Rb_N => Rb_N,
+		addr_io => addr_io,
+		rd_in => rd_in,
+		wr_out => wr_out
+	 );
+	 
+	 ac : multi PORT MAP (
+			clk => clk,
+		   boot => boot,
+         ldpc_l => ldpc_l,
+         wrd_l => wrd_l,
+         wr_m_l => wr_m_l,
+         w_b  => w_b,
+         ldpc => ldpc,
+         wrd => wrd,
+         wr_m => wr_m,
+         ldir => ldir,
+         ins_dad => ins_dad,
+         word_byte => word_byte
+	 );
+	 
+	 -- Salts
+	jmp <= ALU_OUT when (ir_actual(2 downto 0) = "000" and z = '1') or -- JZ
+							 (ir_actual(2 downto 0) = "001" and z = '0') or -- JNZ
+							 (ir_actual(2 downto 0) = "011")				  or -- JMP
+							 (ir_actual(2 downto 0) = "100") else PC2;
+							 
+	tknbr <= INI when boot = '1' else  	 -- BOOT
+				PC_ACT when ldpc = '0' else -- NO ES CARREGA PC
+				BR when ir_actual(15 downto 12) = "0110" and (ir_actual(8) xor z) = '1' else -- HI HA UN BRANCH TAKEN 
+				jmp when ir_actual(15 downto 12) = "1010" else PC2;  -- Els jmp tenen una logica complicada que es calcula a part.
+	 
+	 -- Logica Program Counter
+	 
+	 pc_saltat <= nou_PC+2+std_logic_vector(resize(signed(ir_actual(7 downto 0)&'0'), 16));
+
+	 process(clk)
+	 begin
+		if rising_edge(clk) then
+			case tknbr is 
+				when INI => nou_pc <= x"C000";
+				when ALU_OUT => nou_pc <= aluout;
+				when PC2 => nou_pc <= nou_pc+2;
+				when BR => nou_pc <= pc_saltat;
+				when PC_ACT => NULL;
+				when others => nou_pc <= nou_pc;
+			 end case;
+		 end if;
+	 end process;
+	 pc <= nou_pc;
+	 
+
+	 
+	 -- Cal convertir aixo en un process
+	 ir_actual <= x"0000" when rising_edge(clk) and boot = '1'
+					else datard_m when rising_edge(clk) and ldir = '1';
+					--else ir_actual when rising_edge(clk);
+	 
+
+END Structure;
